4c34edd610a9b4eff86f53badb19fe613e299c2a
anya169 <oxina.anyaff.gmail.com>
2025-03-06 20:30:51 +0700

    адаптив + свайпер 06.06

--- src/scripts/dev/main.js
+++ src/scripts/dev/main.js
@@ -33,7 +33,27 @@
   //       eventPP.addEventListener("click", closeEventPP);
    });
 
-
+  const swipers = document.querySelectorAll(".js-swiper");
+  swipers.forEach(function (swpr) {
+    new Swiper(swpr, {
+      updateOnWindowResize: true,
+      slidesPerView: "auto",
+      freeMode: true,
+      spaceBetween: 0,
+      speed: 500,
+      grabCursor: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+      },
+      navigation: {
+        nextEl: ".swiper-arrow-next",
+        prevEl: ".swiper-arrow-prev",
+        disabledClass: "arrow--disabled"
+      }
+    });
+  });
+  
     const contactsMap = document.querySelector("#js-contactsMap");
     if (contactsMap) {
       const mapStyles = [
@@ -268,27 +288,6 @@
 
     }
     
-    const swipers = document.querySelectorAll(".js-swiper");
-    swipers.forEach(function (swpr) {
-      new Swiper(swpr, {
-        updateOnWindowResize: true,
-        slidesPerView: "auto",
-        freeMode: true,
-        spaceBetween: 0,
-        speed: 500,
-        grabCursor: true,
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true
-        },
-        navigation: {
-          nextEl: ".swiper-arrow-next",
-          prevEl: ".swiper-arrow-prev",
-          disabledClass: "arrow--disabled"
-        }
-      });
-    });
-   
     document.addEventListener('DOMContentLoaded', function() {
     const checkbox = document.getElementById("checkbox10");
     const rectangle = document.getElementById("Rectangle10");
